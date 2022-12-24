8fca90c0f67409e47f37290c89ce32b82ab7f131
Server LTO <www-data@latoucheoriginale.fr>
2022-12-24 00:15:26 +0000

    Update, cleanup

--- src/js/imagine.js
+++ src/js/imagine.js
@@ -1,4 +1,3 @@
-
 $.fn.serializeObject = function () {
 
     var o = {};
@@ -16,7 +15,6 @@ $.fn.serializeObject = function () {
     return o;
 };
 
-
 (function (root, factory) {
 
     if (typeof define === 'function' && define.amd) {
@@ -30,38 +28,43 @@ $.fn.serializeObject = function () {
 })(this, function () {
 
     var Imagine = {};
-    Imagine.version = '0.1.0';
+        Imagine.version = '0.1.0';
 
     var Settings = Imagine.settings = {
 
-        "debug"       : false,
-        "disable"     : false,
+        "debug"           : true,
+        "disable"         : false,
+        "responsive"      : true,
+
+        "lazyload"        : true,
+        "browser-level"   : false, // Prevent lazy loading at browser-level
+        "threshold"       : "25%"
     };
 
-
-    var ready = false;
-
-    Imagine.epsilon = function(x1, x0) { return Math.abs(x1-x0) < 1; }
+    Imagine.epsilon = function(x1, x0) { return (x1-x0) < 1; }
     Imagine.reset = function(el = undefined) {
 
         var targetData = jQuery.data(el || document.documentElement);
         Object.keys(targetData).forEach((key) => delete targetData[key]);
+
+        imageResponsiveList = [];
 
         return this;
     }
 
     Imagine.ready = function (options = {}) {
 
+        if(options instanceof Event) options = {};
+
         if("debug" in options)
             Settings.debug = options["debug"];
 
         Imagine.configure(options);
-        ready = true;
 
         if (Settings.debug) console.log("Imagine is ready.");
-        if (Settings.debug) console.log("(padding = ", Imagine.getScrollPadding(), ")");
         dispatchEvent(new Event('imagine:ready'));
 
+        Imagine.preventLoading();
         return this;
     };
 
@@ -117,16 +120,44 @@ $.fn.serializeObject = function () {
         return this;
     }
 
-    Imagine.onLoad = function (el = window)
+    Imagine.preventLoading = function()
     {
+        if(Imagine.get("browser-level")) return;
+
+        $("img[loading=lazy]").each(function() {
+
+            var src = this.getAttribute("src"); // Keep the original path
+            var lazyload = ($(this).attr("loading") == "lazy");
+
+            if(src && lazyload) {
+
+                if(Settings.debug) console.debug("Imagine prevents loading: ", this);
+                this.setAttribute("data-src", src);
+                this.removeAttribute("src");
+                this.removeAttribute("loading");
+            }
+        });
+    }
+
+    Imagine.onLoad = function (el = undefined)
+    {
+        if(!el) el = document.documentElement;
+        if(el instanceof Event) el = el.target;
+        if(el == window) el = document.documentElement;
+        if(el == document) el = document.documentElement;
+
         if(Imagine.get("disable") === true) {
 
-            $(".imagine").addClass("imagine-disabled");
+            el.find("img.imagine").addClass("disabled");
             return;
         }
 
         Imagine.reset(el);
-        Imagine.onSplit();
+        Imagine.onSplit($(el).find("img.imagine"));
+
+        Imagine.updateImageSet($(el).find("img[data-srcset]"));
+        Imagine.loadImages($(el).find("img[data-src]"));
+
         return this;
     }
 
@@ -158,69 +189,7 @@ $.fn.serializeObject = function () {
         }, new Set());
     }
 
-    Imagine.lazyLoad = function (lazyloadImages = undefined)
-    {
-        lazyloadImages = lazyloadImages || document.querySelectorAll("img[data-src]:not(.loaded)");
-        if ("IntersectionObserver" in window) {
-
-                var imageObserver = new IntersectionObserver(function (entries, observer) {
-                    entries.forEach(function (entry) {
-                        if (entry.isIntersecting) {
-                            var image = entry.target;
-                            var lazybox = image.closest(".lazybox");
-
-                                image.onload = function() {
-                                    this.classList.add("loaded");
-                                    this.classList.remove("loading");
-                                    if(lazybox) lazybox.classList.add("loaded");
-                                    if(lazybox) lazybox.classList.remove("loading");
-                                };
-
-                                if(lazybox) lazybox.classList.add("loading");
-                                image.classList.add("loading");
-                                image.src = image.dataset.src;
-
-                            imageObserver.unobserve(image);
-                        }
-                });
-            });
-
-            lazyloadImages.forEach(function (image) {
-                imageObserver.observe(image);
-            });
-
-        } else {
-
-                var lazyloadThrottleTimeout;
-
-            function lazyload() {
-                if (lazyloadThrottleTimeout) {
-                    clearTimeout(lazyloadThrottleTimeout);
-                }
-
-                lazyloadThrottleTimeout = setTimeout(function () {
-                    var scrollTop = window.pageYOffset;
-                    lazyloadImages.forEach(function (img) {
-                        if (img.offsetTop < (window.innerHeight + scrollTop)) {
-                            img.src = img.dataset.src;
-                            img.classList.add('loaded');
-                        }
-                    });
-                    if (lazyloadImages.length == 0) {
-                        document.removeEventListener("scroll", lazyload);
-                        window.removeEventListener("resize", lazyload);
-                        window.removeEventListener("orientationChange", lazyload);
-                    }
-                }, 20);
-            }
-
-            document.addEventListener("scroll", lazyload);
-            window.addEventListener("resize", lazyload);
-            window.addEventListener("orientationChange", lazyload);
-        }
-    };
-
-    Imagine.loadMedia = function(container = document.documentElement)
+    Imagine.eagerLoad = function(container = document.documentElement)
     {
         function loadImg (src) {
             return new Promise((resolve, reject) => {
@@ -234,8 +203,8 @@ $.fn.serializeObject = function () {
                     })
                 }
 
-                img.onerror = reject
-                img.src = src
+                img.onerror = reject;
+                img.src = src;
             });
         }
 
@@ -257,7 +226,7 @@ $.fn.serializeObject = function () {
 
     Imagine.onSplit = function(images = $("img.imagine")) {
 
-        return Imagine.loadMedia(images).then(function (metadata) {
+        return Imagine.eagerLoad(images).then(function (metadata) {
 
             $(images).each(function(i)
             {
@@ -305,23 +274,270 @@ $.fn.serializeObject = function () {
 
                 $(this).remove();
 
-                $(this).on("click", function() {
-                    $(this).toggleClass("active");
-                });
+                $(this).on("click", function() { $(this).toggleClass("active"); });
 
                 $(window).trigger("load.imagine");
             });
         });
     };
 
-    $(window).on("onbeforeunload", function() {
-        Imagine.reset();
-    });
+    var imageResponsiveList = [];
+    Imagine.updateImageSet = function(images = [])
+    {
+        if(!Imagine.get("responsive")) return;
 
-    $(window).on("load", function() {
-        Imagine.lazyLoad();
-        Imagine.onLoad();
-    });
+        function _payload(image) {
+
+            const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+            const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+            var candidates = [];
+
+            var src = $(image).attr("src") ?? $(image).data("src");
+                src = String(src).trim();
+
+            // Include data-src into data-srcset..
+            var assets = String($(image).data("srcset")).replace(/\s+/g, ' ').trim().split(",");
+            if(!assets.includes(src)) {
+
+                image.setAttribute("data-srcset", src + ", " + $(image).data("srcset"));
+                assets = String($(image).data("srcset")).split(",");
+            }
+
+            assets.forEach(function (entry) {
+
+                if(entry == "undefined" || !entry) return;
+                var array = entry.trim().split(" ");
+
+                var maxHeight = -1;
+                var maxWidth  = -1;
+                if(array.length > 1) {
+
+                    if(array[1].endsWith("w")) maxWidth = parseInt(array[1]);
+                    else maxHeight = parseInt(array[1])
+                }
+
+                if(array.length > 2) {
+
+                    if(array[2].endsWith("w")) maxWidth = parseInt(array[2])
+                    else maxHeight = parseInt(array[2])
+                }
+
+                candidates.push({"src": array[0], "maxWidth": maxWidth, "maxHeight": maxHeight});
+            });
+
+            candidates.sort(function(a, b) {
+
+                if( (a.maxWidth  < 0)        && !(b.maxWidth < 0)         ) return  1;
+                if(!(a.maxWidth  < 0)        &&  (b.maxWidth < 0)         ) return -1;
+                if(a.maxWidth  == b.maxWidth) {
+
+                    if(a.maxHeight == b.maxHeight) return 0;
+                    if( (a.maxHeight  < 0)        && !(b.maxHeight < 0)         ) return  1;
+                    if(!(a.maxHeight  < 0)        &&  (b.maxHeight < 0)         ) return -1;
+                }
+
+                var w = a.maxWidth  > 0 && b.maxWidth  > 0 ? Math.abs(a.maxWidth  - vw) - Math.abs(b.maxWidth  - vw) : 0;
+                var h = a.maxHeight > 0 && b.maxHeight > 0 ? Math.abs(a.maxHeight - vh) - Math.abs(b.maxHeight - vh) : 0;
+                return w == 0 ? h : w;
+            });
+
+            var bestCandidateSrc = candidates[0].src ?? src;
+            if (bestCandidateSrc)
+                image.setAttribute("data-src", bestCandidateSrc);
+
+            return image;
+        }
+
+        // Synchronized loading
+        $(images).each(function() { _payload(this); } );
+
+        // Update on viewport resize
+        window.addEventListener("resize", function() {
+            $(imageResponsiveList).each(function() { _payload(this); });
+            Imagine.loadImages(imageResponsiveList);
+        });
+        window.addEventListener("orientationChange", function() {
+            $(imageResponsiveList).each(function() { _payload(this); });
+            Imagine.loadImages(imageResponsiveList);
+        });
+    }
+
+    Imagine.largestImageLoaded = function(image)
+    {
+        var dataset = $(image).data("srcset");
+        if(!dataset) return true;
+
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+        const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+        var src = $(image).attr("src") ?? $(image).data("src");
+            src = String(src).trim();
+
+        // Include data-src into data-srcset..
+        var assets = String($(image).data("srcset")).replace(/\s+/g, ' ').trim().split(",");
+        if(assets.findIndex((asset) => { return asset.trim().startsWith(src); }, src) < 0)
+        {
+            image.setAttribute("data-srcset", src + ", " + $(image).data("srcset"));
+            assets = String($(image).data("srcset")).split(",");
+        }
+
+        var candidates = [];
+        assets.forEach(function (entry) {
+
+            if(entry == "undefined" || !entry) return;
+            var array = entry.trim().split(" ");
+
+            var maxHeight = -1;
+            var maxWidth  = -1;
+            if(array.length > 1) {
+
+                if(array[1].endsWith("w")) maxWidth = parseInt(array[1]);
+                else maxHeight = parseInt(array[1])
+            }
+
+            if(array.length > 2) {
+
+                if(array[2].endsWith("w")) maxWidth = parseInt(array[2])
+                else maxHeight = parseInt(array[2])
+            }
+
+            candidates.push({"src": array[0], "maxWidth": maxWidth, "maxHeight": maxHeight});
+            candidates.sort(function(a, b) {
+
+                if( (a.maxWidth  < 0)        && !(b.maxWidth < 0)         ) return  1;
+                if(!(a.maxWidth  < 0)        &&  (b.maxWidth < 0)         ) return -1;
+                if(a.maxWidth  == b.maxWidth) {
+
+                    if(a.maxHeight == b.maxHeight) return 0;
+                    if( (a.maxHeight  < 0)        && !(b.maxHeight < 0)         ) return  1;
+                    if(!(a.maxHeight  < 0)        &&  (b.maxHeight < 0)         ) return -1;
+                }
+
+                var w = a.maxWidth  > 0 && b.maxWidth  > 0 ? Math.abs(a.maxWidth ) - Math.abs(b.maxWidth ) : 0;
+                var h = a.maxHeight > 0 && b.maxHeight > 0 ? Math.abs(a.maxHeight) - Math.abs(b.maxHeight) : 0;
+                return w == 0 ? h : w;
+            });
+        });
+
+        if(!candidates.length) return true;
+        return candidates[candidates.length - 1].src == src;
+    }
+
+    var imageResponsiveList = [];
+    Imagine.loadImages = function (images = [])
+    {
+        if(Imagine.get("lazyload")) {
+
+            var lazyImages = images;
+            if(!lazyImages.length) lazyImages = document.querySelectorAll("img[data-src]:not(.loaded)");
+
+            lazyImages = Array.from(lazyImages).filter(i => i.dataset.src);
+            if ("IntersectionObserver" in window) {
+
+                let options = { root:null, rootMargin: Imagine.get("threshold") };
+                var imageObserver = new IntersectionObserver(function (entries, observer) {
+
+                    entries.forEach(function (entry) {
+                        if (entry.isIntersecting) {
+
+                            var image = entry.target;
+                            var lazybox = image.closest(".lazybox");
+
+                            image.onload = function() {
+                                this.classList.add("loaded");
+                                this.classList.remove("loading");
+                                if(lazybox) lazybox.classList.add("loaded");
+                                if(lazybox) lazybox.classList.remove("loading");
+                            };
+
+                            if(lazybox) lazybox.classList.add("loading");
+                            image.classList.add("loading");
+
+                            image.src = image.dataset.src;
+                            imageObserver.unobserve(image);
+
+                            var index = imageResponsiveList.indexOf(image);
+                            if (index !== -1 && Imagine.largestImageLoaded(image))
+                                imageResponsiveList.splice(index, 1);
+                        }
+                    });
+
+                }, options);
+
+                lazyImages.forEach(function (image) {
+                    imageObserver.observe(image);
+                });
+
+            } else {
+
+                var lazyloadThrottleTimeout;
+
+                function lazyload() {
+
+                    if (lazyloadThrottleTimeout) {
+                        clearTimeout(lazyloadThrottleTimeout);
+                    }
+
+                    lazyloadThrottleTimeout = setTimeout(function () {
+
+                        var scrollTop = window.pageYOffset;
+                        images.forEach(function (img) {
+                            if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                                img.src = img.dataset.src;
+                                img.classList.add('loaded');
+                            }
+
+                            if (Imagine.largestImageLoaded(img)) {
+                                var index = imageResponsiveList.indexOf(img);
+                                if (index !== -1) imageResponsiveList.splice(index, 1);
+                            }
+                        });
+
+                        if (images.length == 0) {
+                            document.removeEventListener("scroll", lazyload);
+                            window.removeEventListener("resize", lazyload);
+                            window.removeEventListener("orientationChange", lazyload);
+                        }
+
+                    }, 20);
+                }
+
+                document.addEventListener("scroll", lazyload);
+                window.addEventListener("orientationChange", lazyload);
+            }
+        }
+
+        $("img[data-srcset]").each(function() {
+
+            if (!Imagine.largestImageLoaded(this))
+                imageResponsiveList.push(this);
+
+            if (this.dataset.src)
+                $(this).attr("src", this.dataset.src);
+        });
+    }
+
+    //
+    // Event handling (w/ priority)
+    Imagine.preventLoading(); // Prevent loading lazyload pictures very early
+    window.addEventListener("onbeforeunload", Imagine.reset, true);
+    window.addEventListener("DOMContentLoaded", Imagine.ready);
+    window.addEventListener("load", Imagine.onLoad, false);
+
+    //
+    // LCP Metric
+    if ("IntersectionObserver" in window) {
+
+        new PerformanceObserver((list) => {
+
+            const latestEntry = list.getEntries().at(-1);
+            if (latestEntry?.element?.getAttribute('loading') == 'lazy' || latestEntry?.element?.getAttribute("data-src")) {
+                console.warn('Warning: LCP element was lazy loaded', latestEntry);
+            }
+
+        }).observe({type: 'largest-contentful-paint', buffered: true});
+    }
 
     return Imagine;
 });
